dd4944aa27ab6eab112e33f0c0ce2b3d968f205d
Rezeq Khader <98508228+rezeqk@users.noreply.github.com>
2023-02-17 18:12:54 -0500

    add profile, view profile, check status, and unlock are created

--- app/src/main/java/com/example/coen390_project/Checkstatus.java
+++ app/src/main/java/com/example/coen390_project/Checkstatus.java
@@ -10,5 +10,6 @@ public class Checkstatus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkstatus);
+        getSupportActionBar().setTitle("Status");
     }
 }
--- app/src/main/java/com/example/coen390_project/Viewprofile.java
+++ app/src/main/java/com/example/coen390_project/Viewprofile.java
@@ -10,5 +10,6 @@ public class Viewprofile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewprofile);
+        getSupportActionBar().setTitle("View Profile");
     }
 }
