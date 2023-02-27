e9e6027205c2ea5993205d46bcc4c3362e320ca1
Rezeq Khader <98508228+rezeqk@users.noreply.github.com>
2023-02-27 10:31:52 -0500

    add profile, view profile, check status, and unlock are created

---
+++ app/src/main/java/com/example/coen390_project/MyFirebaseMessagingService.java
@@ -0,0 +1,25 @@
+package com.example.coen390_project;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+    }
+
+
+}
