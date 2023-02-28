c18581de69451dc1ff680668eae039485b2ad166
ladykiller374 <baingheielts@gmail.com>
2023-02-27 19:40:47 -0500

    got the message by installing the onMessageReceived function on MyFirebaseMessagingService Choose any device but with API 31 to run the app

--- app/src/main/java/com/example/coen390_project/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/coen390_project/MyFirebaseMessagingService.java
@@ -28,7 +28,33 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
 
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
 
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+                handleNow();
+            } else {
+                // Handle message within 10 seconds
+                handleNow();
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
     // [START on_new_token]
     /**
      * There are two scenarios when onNewToken is called:
@@ -48,6 +74,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // FCM registration token to your app server.
         sendRegistrationToServer(token);
     }
+
+    private void handleNow() {
+        Log.d(TAG, "Short lived task is done.");
+    }
+
 
     private void sendRegistrationToServer(String token) {
         // TODO: Implement this method to send token to your app server.
