5cc916d5a1d39847147ef0e5a88a528def66b62d
Theebika6 <82008678+Theebika6@users.noreply.github.com>
2023-03-26 23:34:34 -0400

    Copied paste the code from view_profile to history and changed the firebase child to "History"

--- app/src/main/java/com/example/coen390_project/Views/History.java
+++ app/src/main/java/com/example/coen390_project/Views/History.java
@@ -1,17 +1,82 @@
 package com.example.coen390_project.Views;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.example.coen390_project.Models.User;
 import com.example.coen390_project.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 
 public class History extends AppCompatActivity {
+
+    ListView list_view_history;
+
+    DatabaseReference reference;
+
+
+    String TAG = "TAG";
+
+    final ArrayList<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
         getSupportActionBar().setTitle("History");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        list_view_history= findViewById(R.id.listview_history);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.list_profile,list);
+        list_view_history.setAdapter(arrayAdapter);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("History");
+
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String user = snapshot.getValue(String.class);
+                list.add(user);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
